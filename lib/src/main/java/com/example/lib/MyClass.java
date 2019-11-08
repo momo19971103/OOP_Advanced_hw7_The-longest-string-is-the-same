c1a26bf1c601bb1b09333be0bba48572231ca689
momo19971103 <momo19971103@yahoo.com.tw>
2019-11-08 23:59:53 +0800

    新增註解

--- lib/src/main/java/com/example/lib/MyClass.java
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -8,33 +8,33 @@ public class MyClass {
         System.out.println("請輸入一字串(<100)");
         String inputstring = scanner.next();
         String LongestStandard = "";
-        int LongestLength=0;
+        int LongestLength = 0;
         System.out.println(inputstring);
         System.out.println(inputstring.substring(1, 3));
         int InputstringLength = inputstring.length();
 
         for (int i = 0; i < InputstringLength; ) {
-            for (int j = 2; j < InputstringLength + 1; ) {
+            for (int j = 2; j < InputstringLength + 1; ) {                      //(若由後往前找得值等同起始值，可知沒有一個相同)
                 if (inputstring.lastIndexOf(inputstring.substring(i, j)) == i) {//後面沒有相同的
-                    i++;
-                    j = i + 2;
+                    i++;                                                //往下找
+                    j = i + 2;                                          //倆倆先找
                     if (j >= InputstringLength + 1) {
-                        i = InputstringLength;
+                        i = InputstringLength;                          //字串收尋結束直接強迫結束迴圈
                     }
                 } else {
-                    int LN=inputstring.substring(i, j).length();
-                    if ( LN> LongestLength) {
-                        LongestStandard = inputstring.substring(i, j);
-                        LongestLength=LN;
-                    }
-                    else if( LN== LongestLength){
-                        LongestStandard=LongestStandard+","+inputstring.substring(i, j);
+                    int LN = inputstring.substring(i, j).length();
+                    if (LN > LongestLength) {                           //如果有相同長度的字串大於"最長字串長度"
+                        LongestStandard = inputstring.substring(i, j);  //將字串覆蓋於標準字串
+                        LongestLength = LN;                             //將"最長字串長度"填入該長度
+                    } else if (LN == LongestLength) {                   //若有長度相同
+                        LongestStandard = LongestStandard + "," +       //進行字串增長
+                                inputstring.substring(i, j);
                     }
                     j++;
                 }
             }
         }
-        System.out.println("最常重複字串:"+LongestStandard);
+        System.out.println("最常重複字串:" + LongestStandard);
 
     }
 
